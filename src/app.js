adb1ace6423d538a45fbed9e22938647ad2f6283
Tyson Williams <tyson.j.williams@gmail.com>
2018-03-09 12:04:22 -0700

    Setup config file
    Config still needs API key (next step to do)
    
    Refactor port to come from config.port

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+const config = require('./config.js');
 const express = require('express');
 const fetch = require('node-fetch');
  
 const app = express();
-const PORT = 3000;
+const PORT = config.port;
  
 app.listen(PORT, err => {
   if (err) {
